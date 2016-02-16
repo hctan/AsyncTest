a4abb2abf587e6ea126594ef13b113076abdd58b
Han Chong <hanchong@senatraffic.com.my>
2016-02-16 10:49:02 +0800

    Initial version

---
+++ AsyncTest/AsyncFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncTest
+{
+    public class AsyncFactory
+    {
+        public static Task<int> GetIntAsync(CancellationToken token = default(CancellationToken))
+        {
+            var tcs = new TaskCompletionSource<int>();
+
+            if (token.IsCancellationRequested)
+            {
+                tcs.SetCanceled();
+                return tcs.Task;
+            }
+
+            var timer = new System.Timers.Timer(3000);
+            timer.AutoReset = false;
+            timer.Elapsed += (s, e) =>
+            {
+                tcs.TrySetResult(10);
+                timer.Dispose();
+            };
+
+            if (token.CanBeCanceled)
+            {
+                token.Register(() => {
+                    tcs.TrySetCanceled();
+                    timer.Dispose();
+                });
+            }
+
+            timer.Start();
+            return tcs.Task;
+        }
+    }
+}
---
+++ AsyncTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            cts.CancelAfter(1000);
+
+            AsyncFactory.GetIntAsync(cts.Token).ContinueWith((task) => {
+                //We get the response.
+                //So dispose the CancellationTokenSource
+                //so that it is not going to signal.
+                cts.Dispose();
+
+                if (task.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine(task.Result);
+                }
+                else if (task.Status == TaskStatus.Canceled)
+                {
+                    Console.WriteLine("The task has been canceled.");
+                }
+                else
+                {
+                    Console.WriteLine("An error has been occurred. Details:");
+                    Console.WriteLine(task.Exception.InnerException.Message);
+                }
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
